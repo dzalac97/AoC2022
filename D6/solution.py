4d2d34367313d7826961a025b6051f6ac1e6dead
dzalac97 <zalacdomagoj@gmail.com>
2022-12-06 15:04:55 +0100

    Refactor

--- D6/solution.py
+++ D6/solution.py
@@ -3,15 +3,13 @@ Created Date: Tuesday, December 6th 2022, 2:47:04 pm
 Author: Domagoj Žalac
 '''
 
+def findStartOfMarker(signal, distNum):
+    for i in range(len(signal) - distNum):
+        seq = signal[i : i + distNum]
+        if len(set(seq)) == distNum:
+            return i + distNum
+
 with open('input', 'r') as file:
     signal = file.readline().strip()
-    for i in range(len(signal) - 4):
-        seq = signal[i : i + 4]
-        if len(set(seq)) == 4:
-            print('1st star:', i + 4)
-            break
-    for i in range(len(signal) - 14):
-        seq = signal[i : i + 14]
-        if len(set(seq)) == 14:
-            print('2nd star:', i + 14)
-            break
+    print('1st star:', findStartOfMarker(signal, 4))
+    print('2nd star:', findStartOfMarker(signal, 14))
