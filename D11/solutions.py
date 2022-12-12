002421c49615507938cbf3e8b32d601cb4616325
dzalac97 <zalacdomagoj@gmail.com>
2022-12-12 15:21:37 +0100

    Day 11 2nd star

--- D11/solutions.py
+++ D11/solutions.py
@@ -6,8 +6,9 @@ Author: Domagoj Žalac
 import operator
 import itertools as it
 from math import prod
+from copy import deepcopy
 
-ops = { "*": operator.mul, "+": operator.add }
+ops = { '*': operator.mul, '+': operator.add, '%': operator.mod, '/': operator.truediv }
 
 class Monkey:
     def __init__(self, items, operation, division):
@@ -22,7 +23,7 @@ class Monkey:
         operator, value = self._operation[0], self._operation[1]
         if value == 'old':
             value = old
-        return int(ops[operator](old, int(value)) / 3)
+        return int(ops[worry[0]](ops[operator](old, int(value)), worry[1]))
 
     def _testDivisibility(self, item, monkeys):
         if item % self._div == 0:
@@ -42,20 +43,31 @@ class Monkey:
 
 with open('input', 'r') as file:
     monkeys = []
-
+    
+    # Least Common Multiple is the smallest number that is a multiple of both
+    LCM = ['%', 1]
     for key,group in it.groupby(file, lambda line: line.startswith('Monkey')):
         if not key:
             monkey = [line.strip() for line in group]
             items = [int(x) for x in monkey[0][len('Starting items: '):].split(',')]
             operation = monkey[1][len('Operation: new = old '):].split()
             div = int(monkey[2][len('Test: divisible by '):])
+            LCM[1] *= div
             true = int(monkey[3][len('If true: throw to monkey '):])
             false = int(monkey[4][len('If false: throw to monkey '):])
             division = [div, true, false]
             monkeys.append(Monkey(items, operation, division))
 
+    monkeys2 = deepcopy(monkeys)
+    
+    worry = ['/', 3]
     for round in range(1, 21):
         for monkey in monkeys:
             monkey.takeTurn(monkeys)
-
     print('1st star:', prod(sorted([monkey.getInspectedItemsCount() for monkey in monkeys], reverse=True)[:2]))
+    
+    worry = LCM
+    for round in range(1, 10001):
+        for monkey in monkeys2:
+            monkey.takeTurn(monkeys2)
+    print('2nd star:', prod(sorted([monkey.getInspectedItemsCount() for monkey in monkeys2], reverse=True)[:2]))
